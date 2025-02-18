f6824c809a03dd14b4bcad6bac9704e6c1931ce6
Alen Oviedo <afoviedola@gmail.com>
2025-02-18 12:00:38 -0500

    Estructura del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddTaskComponent } from "./components/add-task/add-task.component";
+import { ListTaskComponent } from './components/list-task/list-task.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, AddTaskComponent, ListTaskComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ToDo-list';
+    
 }
---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,12 @@
+import { AfterContentInit, Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-add-task',
+    standalone: true,
+    imports: [],
+    templateUrl: './add-task.component.html',
+    styleUrl: './add-task.component.scss'
+})
+export class AddTaskComponent {
+
+}
