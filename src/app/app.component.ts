093fad97bc657bf0767f291483349db8bb559ee8
Alen Oviedo <afoviedola@gmail.com>
2025-02-18 15:26:23 -0500

    Instalación de Material, configuración componente add-task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddTaskComponent } from "./components/add-task/add-task.component";
 import { ListTaskComponent } from './components/list-task/list-task.component';
+import { provideNativeDateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [RouterOutlet, AddTaskComponent, ListTaskComponent],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
+    providers: [
+        provideNativeDateAdapter(), // Proveedor de adaptación de fechas nativo
+        { provide: MAT_DATE_LOCALE, useValue: 'es-ES' } // Configuración del idioma opcional
+    ]
 })
 export class AppComponent {
-    
+
 }
--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,9 +1,15 @@
-import { AfterContentInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'app-add-task',
     standalone: true,
-    imports: [],
+    imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule, MatCheckboxModule, MatButtonModule],
     templateUrl: './add-task.component.html',
     styleUrl: './add-task.component.scss'
 })
