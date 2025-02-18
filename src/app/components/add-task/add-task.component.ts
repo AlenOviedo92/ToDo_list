99cbeabc84184646d637e8374988f91bc9d2a4d1
Alen Oviedo <afoviedola@gmail.com>
2025-02-18 17:06:44 -0500

    Recupero los datos del formulario y agrego estilos preliminares

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -5,14 +5,38 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-add-task',
     standalone: true,
-    imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule, MatCheckboxModule, MatButtonModule],
+    imports: [
+        MatFormFieldModule, 
+        MatInputModule, 
+        MatDatepickerModule, 
+        MatSelectModule, 
+        MatCheckboxModule, 
+        MatButtonModule,
+        ReactiveFormsModule
+    ],
     templateUrl: './add-task.component.html',
     styleUrl: './add-task.component.scss'
 })
 export class AddTaskComponent {
+    taskForm: FormGroup;
 
+    constructor(private fb: FormBuilder) {
+        this.taskForm = this.fb.group({
+            title: [''],
+            dueDate: [''],
+            priority: [''],
+            description: [''],
+            isRecurring: [false]
+        });
+    }
+
+    addTask(): void {
+        console.log('Datos del formulario: ', this.taskForm.value);
+    }
 }
