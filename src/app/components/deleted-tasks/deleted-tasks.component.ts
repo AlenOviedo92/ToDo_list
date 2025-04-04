dd994656e042fa1ea4d3134f716985e0223e7446
Alen Oviedo <afoviedola@gmail.com>
2025-04-04 12:43:39 -0500

    Configuro el paginado en la tabla de tareas eliminadas

--- src/app/components/deleted-tasks/deleted-tasks.component.ts
+++ src/app/components/deleted-tasks/deleted-tasks.component.ts
@@ -1,8 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { TaskService } from '../../services/task.service';
 import { ITask } from '../../models/tasks';
 import { Router } from '@angular/router';
 
@@ -18,13 +19,28 @@ import { Router } from '@angular/router';
     templateUrl: './deleted-tasks.component.html',
     styleUrl: './deleted-tasks.component.scss'
 })
-export class DeletedTasksComponent {
+export class DeletedTasksComponent implements OnInit, AfterViewInit {
     displayedColumns: string[] = ['position', 'task', 'priority', 'description', 'date', 'recurring', 'actions'];
     dataSource = new MatTableDataSource<ITask>([]);
+    originalTasks: ITask[] = [];                                
 
     @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+    ngOnInit(): void {
+        this.taskService.getTask();
+        this.taskService.tasks$.subscribe(tasks => {
+            this.originalTasks = tasks ?? [];
+            this.dataSource.data = this.originalTasks;
+            this.dataSource.paginator = this.paginator;
+        });
+    }
+
+    ngAfterViewInit(): void {
+        this.dataSource.paginator = this.paginator;
+    }
     
     constructor(
+        private taskService: TaskService,
         private router: Router,
     ) {}
 
